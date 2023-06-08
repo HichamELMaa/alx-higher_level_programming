8f6c09bc5804c51533dd1edbd05b49fd59311813
HichamELMaa <hm.elmaataoui@gmail.com>
2023-06-08 08:34:03 -0700

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,11 @@
-#!/usr/bin/python3
-
 import sys
 
+def add_arguments(args):
+    result = 0
+    for arg in args:
+        result += int(arg)
+    return result
+
 if __name__ == "__main__":
-    result = sum(int(arg) for arg in sys.argv[1:])
+    result = add_arguments(sys.argv[1:])
     print(result)
