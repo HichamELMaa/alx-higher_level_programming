6b460fa8890fb3ef739a36db903fb232b00a5de4
HichamELMaa <hm.elmaataoui@gmail.com>
2023-07-20 05:44:03 -0700

    python almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,7 +9,7 @@ class Base:
     This class will be the "base" for all other classes in this project.
 
     Private Class Attributes:
-        __nb_object (int): Number of instantiated Bases.
+        __nb_objects (int): Number of instantiated Bases.
     """
 
     __nb_objects = 0
@@ -25,4 +25,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-            
